d322bd11eca488eaefea76b0e721601a093eead7
krisa <krisa@krisa.cz>
2016-03-11 17:00:54 +0100

    First commit

---
+++ dokuwiki/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+krisa:$1$LuU3TrSE$fE5H1qCy/XbC/L56Sg3XW0:krisa:krisa@krisa.cz:admin,user
